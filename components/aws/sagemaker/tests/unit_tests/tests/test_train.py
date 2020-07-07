98105504353a5247f264a8072e3df1849fae9d77
Dustin Luong <dusluong@amazon.com>
2020-07-07 14:42:02 -0700

    Updated unit tests, working on integration tests

--- components/aws/sagemaker/tests/unit_tests/tests/test_train.py
+++ components/aws/sagemaker/tests/unit_tests/tests/test_train.py
@@ -80,12 +80,6 @@ class TrainTestCase(unittest.TestCase):
       ResourceConfig={'InstanceType': 'ml.m4.xlarge', 'InstanceCount': 1, 'VolumeSizeInGB': 50, 'VolumeKmsKeyId': ''},
       RoleArn='arn:aws:iam::123456789012:user/Development/product_1234/*',
       StoppingCondition={'MaxRuntimeInSeconds': 3600},
-      DebugHookConfig={
-          'HookParameters':{},
-          'LocalPath':'',
-          'S3OutputPath':''
-      },
-      DebugRuleConfigurations=[],
       Tags=[],
       TrainingJobName='test-job'
     )
